18a17695b50db91dd501f8af6ae8c92897d7bbfd
Oleg <moonolgerd@gmail.com>
2019-07-22 21:59:38 -0400

    Added ToDo

---
+++ Proba.Client/Models/TodoItem.cs
@@ -0,0 +1,23 @@
+﻿namespace Proba.Models
+{
+    public class TodoItem
+    {
+        private bool isDone;
+
+        public string Title { get; set; }
+        public bool IsDone
+        {
+            get => isDone;
+            set
+            {
+                isDone = value;
+                TextDecoration = "line-through";
+                Visibility = "collapse";
+            }
+        }
+
+        public string TextDecoration { get; private set; }
+
+        public string Visibility { get; private set; } = "visible";
+    }
+}
